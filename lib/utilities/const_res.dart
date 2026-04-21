8d8154c33718bd89e6d41019158a8e3c73c82142
Flayr2003 <flayr2003@gmail.com>
2026-04-21 16:11:30 +0200

    Update const_res.dart

--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -1,9 +1,9 @@
-const String baseURL = 'https://fistayl.com/';
+فخconst String baseURL = 'https://fistayl.com/';
 const String apiURL = '${baseURL}api/';
 const String apiKey = 'retry123';
 
 // If you change this topic you also change backend .env file
 String notificationTopic = "shortzz";
 
-String revenueCatAndroidApiKey = "______"; // revenueCat android api
-String revenueCatAppleApiKey = "________"; // revenueCat apple api
+String revenueCatAndroidApiKey = ""; // revenueCat android api
+String revenueCatAppleApiKey = ""; // revenueCat apple api
